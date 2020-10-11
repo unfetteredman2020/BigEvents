f418a5d9c8562f814141f036c437cbd1e971a7c3
unfetteredman <weixw2014@qq.com>
2020-10-11 19:09:47 +0800

    保存token，登录跳转

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,7 @@
 $(function() {
+    const { form, layer } = layui;
+    // const layer = layui.layer;
+
     // 去登录
     $('#link_login').on('click', function() {
         $('.reg-box').hide()
@@ -9,9 +12,10 @@ $(function() {
         $('.reg-box').show()
         $('.login-box').hide()
     });
-    const { form } = layui;
+
+    // const form = layui.form;
     form.verify({
-        pwd: [/^[\S]{6,12}/, '密码6-12位，不能出现空格'],
+        'pwd': [/^[\S]{6,12}/, '密码6-12位，不能出现空格'],
         repwd(value) {
             var pwd = $('.reg-box [name=password]').val()
             if (value !== pwd) {
@@ -19,4 +23,36 @@ $(function() {
             }
         }
     });
+    //注册请求
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault()
+        $.post('/api/reguser', {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }, function(res) {
+            if (res.status !== 0) {
+                layer.msg(res.message);
+                return;
+            }
+            layer.msg(res.message);
+            $('#link_login').click();
+        })
+    });
+    //登录请求
+    $('#form_login').on('submit', function(e) {
+        e.preventDefault()
+        $.post('/api/login', {
+            username: $('#form_login [name=username]').val(),
+            password: $('#form_login [name=password]').val()
+        }, function(res) {
+            if (res.status !== 0) {
+                layer.msg(res.message);
+                return
+            }
+            layer.msg(res.message);
+            //保存token
+            localStorage.setItem('token', res.token);
+            location.href = './index.html'
+        })
+    })
 })
